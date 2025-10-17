fbbdfa748ac370c25b558a3efabecb2156df9f2b
rreynaleon <ro.reyna83@gmail.com>
2025-10-17 16:11:52 +0200

    View dashboard

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -14,6 +14,24 @@
         echo htmlspecialchars(implode(', ', $labels)).".";
         ?>
     </p>
+    <?php
 
-    <a href="/user/profile">Voir mon profil</a><br>
+    $isPassenger = in_array(2, $userRoles, true);
+    $isDriver = in_array(3, $userRoles, true);
+
+    if ($isPassenger && $isDriver) {
+        echo '<p>Vous êtes à la fois passager et conducteur. Vous pouvez accéder à votre profil et gérer votre voiture.</p>';
+        echo '<a href="/user/profile">Voir mon profil</a><br>';
+        echo '<a href="/user/car">Gérer ma voiture</a><br>';
+    } elseif ($isPassenger) {
+        echo '<p>Vous êtes un passager. Vous pouvez accéder à votre profil.</p>';
+        echo '<a href="/user/profile">Voir mon profil</a><br>';
+    } elseif ($isDriver) {
+        echo '<p>Vous êtes un conducteur. Vous pouvez éditer votre profil et votre voiture.</p>';
+        echo '<a href="/user/profile">Éditer mon profil</a><br>';
+        echo '<a href="/user/car">Gérer ma voiture</a><br>';
+    }
+    ?>
+    <?php else: ?>
+        <p>Vous n'êtes pas connecté. Veuillez vous connecter pour accéder à votre tableau de bord.</p>
 <?php endif; ?>
